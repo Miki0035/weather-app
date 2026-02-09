924c21cc1dfff2d5535aec40511cb14132a0e36f
Mikiyas Kebede <mknewkebede@gmail.com>
2026-02-09 16:32:34 +0300

    feat: implement custom search ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import Navbar from "./components/Navbar";
+import { Searchbar } from "./components/Searchbar";
 
 const App = () => {
   return (
     <>
       <Navbar />
+      <main className="mx-auto max-w-7xl">
+        <div className="flex flex-col items-center gap-12">
+          <h1 className="font-bricolage font-bold text-5xl text-white text-center tracking-wide">
+            How's the sky looking today?{" "}
+          </h1>
+          {/* Searcbar */}
+          <Searchbar />
+        </div>
+      </main>
     </>
   );
 };
